ec53521208ce785aba6fc6e569b5dbfc09e0357d
Master0701 <matthias_jessen@web.de>
2026-07-11 02:56:42 +0200

    MediaHub v1.0.4

--- src/mediahub/app_info.py
+++ src/mediahub/app_info.py
@@ -1,5 +1,5 @@
 APP_NAME = "MediaHub"
-APP_VERSION = "1.0.3"
+APP_VERSION = "1.0.4"
 APP_AUTHOR = "MediaHub"
 APP_DESCRIPTION = "MediaHub - YouTube Kanal-, Playlist- und Download-Verwaltung"
 APP_COPYRIGHT = "Copyright © 2026 MediaHub"
